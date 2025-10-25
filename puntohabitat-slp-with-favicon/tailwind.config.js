601a9edda123e29227aed05b107b53e9e260f7f6
grupodj2022-boop <grupodj2022@gmail.com>
2025-10-25 15:05:37 -0600

    Delete puntohabitat-slp-with-favicon directory

--- puntohabitat-slp-with-favicon/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: { colors: { brand: '#111827' } } },
-  plugins: [],
-};
